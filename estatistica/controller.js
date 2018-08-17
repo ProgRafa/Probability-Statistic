c01e69d0a97892159de41fc8081c89ec8df69fe9
ProgRafa <progr4mm3r4af43l@gmail.com>
2018-08-17 16:00:12 -0300

    update da classe controller
    adicionado métodos de manipulação a classe controller.

--- estatistica/controller.js
+++ estatistica/controller.js
@@ -1,22 +1,10 @@
 var Controller;
-var oController;
+var SESSION = sessionStorage;
+var LOCAL_STORAGE = window.localStorage;
 
-function Controller(){
-	this.title;
-	this.amostra;
-	this.ocorrencia;
-	this.probabilidade;
-	
-	this.goToGraphic = function(){
-		this.titulo = document.getElementById('titulo').value;
-		this.amostra = document.getElementById('amostra').value;
-		this.ocorrencia = document.getElementById('ocorrencia').value;
-		this.probabilidade = document.getElementById('prob').value;
-		
-		window.location.href = "graphic.html?title=" + this.titulo + 
-											"&amostra=" + this.amostra +
-											"&ocorrencia=" + this.ocorrencia+
-											"&probabilidade=" + this.probabilidade;
+Controller = function(){
+	this.goTo = function(url){
+		window.location.href = url;
 	}
 	
 	this.parseUrl = function(){
@@ -32,10 +20,80 @@ function Controller(){
 			
 			params[pair.split('=')[0]] = pair.split('=')[1];
 		}
+	}
+	
+	this.iniciarSessao = function(usuario){
+		SESSION.setItem('user', usuario);		
+	}
+	
+	this.addItemSession = function(chave, valor){
+		SESSION.setItem(chave, valor);
+	}
+	
+	this.removerItemSessao = function(chave, valor){
+		SESSION.removeItem(chave);
+	}
+	
+	this.addItemLocal = function(chave, valor){
+		LOCAL_STORAGE.setItem(chave, valor);
+	}
+	
+	this.removerItemLocal = function(chave){
+		LOCAL_STORAGE.removeItem(chave);
+	}
+	
+	this.limparSessao = function(){
+		SESSION.clear();
+	}
+	
+	this.limparLocal = function(){
+		LOCAL_STORAGE.clear();
+	}
+	
+	this.getId = function(id){
+		return document.getElementById(id);
+	}
+	
+	this.setAttr = function(id, attr, value){
+		document.getElementById(id).setAttribute(attr, value);
+	}
+	
+	this.getAttr = function(id, attr){
+		document.getElementById(id).getAttribute(attr);
+	}
+	
+	this.addEvent = function(evento, id, callback){
+		document.getElementById(id).addEventListener(evento, callback);
+	}
+	
+	this.menuEfeito = function(elemento, event){
+		var selecionado = document.getElementsByClassName('hMenuSelecionado')[0];
+		if(event.type == 'mouseover'){
+			elemento.className += ' mouseOver';
+		}
 		
-		this.title = params.title;
-		this.amostra = params.amostra;
-		this.ocorrencia = params.ocorrencia;
-		this.probabilidade = params.probabilidade;
+		if(event.type == 'mouseout'){
+			elemento.className = this.removeClasse('mouseOut', elemento);
+		}
+		
+		if(event.type == 'click'){
+			var classes = elemento.className.split(' ');
+			classes.pop();
+			classes.pop();
+			
+			if(selecionado != undefined){
+				selecionado.className = this.removeClasse('hMenuSelecionado', selecionado);
+				selecionado.className += ' hMenuPadrao';
+			}
+				
+			elemento.className = classes.join(' ').toString();
+			elemento.className += ' hMenuSelecionado -';
+		}
+	}
+	
+	this.removeClasse = function(nome, elemento){
+		var classes = elemento.className.split(' ');
+		classes.splice(classes.findIndex(a => nome == a), 1);
+		return classes.join(' ').toString();
 	}
 }
